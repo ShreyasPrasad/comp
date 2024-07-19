30a0eaaf2082ad5c3a059d2face94015288976d9
Shreyas Prasad <shreyaskrishnaprasad@gmail.com>
2024-07-19 01:48:44 -0700

    Setup basic solution template

---
+++ alias.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+typedef unordered_set<int> usi;
+typedef unordered_set<string> uss;
---
+++ solve.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "alias.h"
+
+int main() {
+	vi thing(2,0);
+	usi h;
+	cout << thing.at(0) << endl;
+}
